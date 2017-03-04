46b4f4d120447dff680ee49d0b711f5cf2756920
shtoorman <bar2683@mail.ru>
2017-03-04 14:21:40 +0200

    add method run

---
+++ Sourse Files/components/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Router
+{
+    //массив с маршрутами
+    private $routes;
+
+    public function __construct()
+    {
+    }
+
+    public function run(){
+
+        echo 'Class Router, method run';
+    }
+}
